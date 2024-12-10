c286d20dfca069ebc90d126ba52713b8b20829bd
Luiz-lang <oluizde01@gmail.com>
2024-12-10 13:20:33 -0300

    Add files via upload

---
+++ Exemplo-de-manipulacao-de-DOM/script.js
@@ -0,0 +1,42 @@
+// Seleciona os elementos do DOM que serão manipulados
+const botao = document.getElementById("btnInteragir");
+const mensagem =  document.getElementById("mensagem");
+
+// Adiciona um evento de clique ao botão
+botao.addEventListener("click", () =>{
+    // Solicita o nome do usuário
+    let nome  = prompt("Qual é o seu nome?");
+
+    // Condição para verificar se o nome foi fornecido
+    if (nome) {
+
+    // Solicita a idade do usuário
+    let  idade = prompt("Qual é a sua idade?");
+
+    // Condição para verificar se a idade é um número válido
+    if (isNaN(idade) || idade <= 0){
+        mensagem.innerText = "Por favor, insira uma  idade válida.";
+        mensagem.style.color  = "red";
+        // Altera a cor do texto para vermelho
+    }  else {
+        // Saída de dados com base na idade fornecida
+        if (idade < 18) {
+            mensagem.innerText = `Olá, ${nome}! Você tem apenas ${idade} anos.`;
+            mensagem.style.color  = "blue";
+            // Altera a cor do texto para azul
+        } else {
+            mensagem.innerText = `Olá, ${nome}! Você é maior de idade com ${idade} anos`;
+            mensagem.style.color  = "green";
+            // Altera a cor do texto para verde
+        }
+        console.log(`Nome do usuário é ${nome}`);
+        console.log(`Idade do usuário é ${idade}`);
+    }
+
+    } else{
+        mensagem.innerText = "Nome não fornecido. Por favor, insira um nome."
+        mensagem.style.color  = "orange";
+        // Altera a cor do texto para laranja
+    }
+
+});
